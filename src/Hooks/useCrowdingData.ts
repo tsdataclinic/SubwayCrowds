af278a2c7e295908b670f983be4c8ea2c9f91566
Kaushik Mohan <kaushik.s.mohan@gmail.com>
2020-07-10 12:47:45 -0400

    loading in crlowding csv

--- src/Hooks/useCrowdingData.ts
+++ src/Hooks/useCrowdingData.ts
@@ -1,17 +1,38 @@
 import {useState,useEffect} from 'react'
+import Papa from 'papaparse'
 
 type CrowdingDatum = {
     x:Number,
     y:Number
 }
 
+type RawCrowding={
+    STATION: string,
+    route_id: string,
+    hr: Number,
+    crowd: Number
+}
+
 export const useCrowdingData = (station :any , line:any)=>{
     const [crowdingData,setCrowdingData] = useState<CrowdingDatum[] | null >(null)
+
+    const standardizeCrowding = (data:any)=>{
+        console.log(data)
+    }
+
+
     useEffect(()=>{
         if(station && line){
             const data = [...Array(24)].map((_:any, i:Number) => ({x:i, y: Math.random()*30}) )
             setCrowdingData(data)
         }
     },[station,line])
+    useEffect( ()=>{
+        Papa.parse('https://raw.githubusercontent.com/tsdataclinic/mta-accessibility/crowding_analysis/analysis/train_crowding/data/Apr_crowd_estimates.csv?token=AC5ZOMVWWASL6UOOGA6YOIS7CGSUS',{
+            download:true,
+            complete: (data :any)=> standardizeCrowding(data.data),
+            header:true
+        })
+    },[])
     return crowdingData
 }
