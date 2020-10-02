b68caad169f2687ad4a2170f7a39a5a27ba151a5
Stuart Lynn <stuart.lynn@gmail.com>
2020-10-02 13:20:21 -0400

    turning password back on

--- src/App.tsx
+++ src/App.tsx
@@ -137,7 +137,7 @@ function App() {
     }
   }, [startStationID, endStationID, selectedLineID, loadedParams]);
 
-  const requirePassword = false;
+  const requirePassword = true;
   if (passwordPassed === false && requirePassword) {
     return (
       <div className="App">
