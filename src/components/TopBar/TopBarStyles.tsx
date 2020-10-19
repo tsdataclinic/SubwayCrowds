6804873622df017ebcebbf316ca42793b514a787
Stuart Lynn <stuart.lynn@gmail.com>
2020-10-19 10:02:37 -0400

    making the top bar larger

--- src/components/TopBar/TopBarStyles.tsx
+++ src/components/TopBar/TopBarStyles.tsx
@@ -1,42 +1,42 @@
-import styled from "styled-components"
-
+import styled from "styled-components";
 
 const TopBar = styled.div`
-   width:100%;
-   height:20px;
-   /* box-sizing:border-box; */
-   padding:20px 0px;
-   display:flex;
-   flex-direction:row;
-   justify-content:space-between;
-   align-items:center;
-   button{
-     margin-right: 5px;
-   }
-`
+  width: 100%;
+  height: 20px;
+  /* box-sizing:border-box; */
+  padding: 20px 0px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 25px;
+  button {
+    margin-right: 5px;
+    font-size: 20px;
+  }
+`;
 const ModalButton = styled.button`
-  background:none;
-  border:none;
-  text-decoration:underline;
-  cursor:pointer;
-`
+  background: none;
+  border: none;
+  cursor: pointer;
+`;
 const Links = styled.div`
-display:flex;
-flex-direction:row;
-align-items:center
-`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
 const DataClinicLink = styled.a`
- display:flex;
- color:black;
- text-decoration:none;
- flex-direction:row;
- align-items:center;
- color: #1e4d5e;
-`
+  display: flex;
+  color: black;
+  text-decoration: none;
+  flex-direction: row;
+  align-items: center;
+  color: #1e4d5e;
+`;
 
-export const Styles={
-    TopBar,
-    ModalButton,
-    DataClinicLink,
-    Links
-} 
+export const Styles = {
+  TopBar,
+  ModalButton,
+  DataClinicLink,
+  Links,
+};
