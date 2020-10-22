b27c62edd6047507194f8972a0cc078641caef26
Stuart Lynn <stuart.lynn@gmail.com>
2020-10-22 16:34:25 -0400

    Turning on password

--- src/App.tsx
+++ src/App.tsx
@@ -163,7 +163,7 @@ function App() {
     }
   }, [startStationID, endStationID, selectedLineID, loadedParams]);
 
-  const requirePassword = false;
+  const requirePassword = true;
   if (passwordPassed === false && requirePassword) {
     return (
       <div className="App">
