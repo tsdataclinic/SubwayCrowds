1bafc3e84befeaa4036c6f20320144c2db5fda66
Stuart Lynn <stuart.lynn@gmail.com>
2020-10-22 16:17:39 -0400

    adding tracking to about and feedback pages

--- src/components/FeedbackModal/FeedbackModal.tsx
+++ src/components/FeedbackModal/FeedbackModal.tsx
@@ -1,43 +1,45 @@
-import React from 'react'
-import Modal from 'react-modal'
-import { Styles } from './FeedbackModalStyles'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import React, { useEffect } from "react";
+import Modal from "react-modal";
+import { Styles } from "./FeedbackModalStyles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import * as Fathom from "fathom-client";
 
 const customStyles = {
-    overlay: { zIndex: 1000 },
-    content: {
-        maxWidth: "60vw",
-        left: "20vw",
-        maxHeight: "80vh",
-        top: "10vh"
-    }
-}
+  overlay: { zIndex: 1000 },
+  content: {
+    maxWidth: "60vw",
+    left: "20vw",
+    maxHeight: "80vh",
+    top: "10vh",
+  },
+};
 
-Modal.setAppElement('#root')
+Modal.setAppElement("#root");
 type FeedbackModalProps = {
-    isOpen: boolean,
-    onClose: () => void
-}
+  isOpen: boolean;
+  onClose: () => void;
+};
 export function FeedbackModal({ isOpen, onClose }: FeedbackModalProps) {
+  useEffect(() => {
+    if (isOpen) {
+      Fathom.trackPageview({ url: "/feedback" });
+    }
+  }, [isOpen]);
 
-    return (
-        <Modal isOpen={isOpen}
-            onRequestClose={onClose}
-            style={customStyles}
-        >
-            <Styles.Container>
-                <Styles.Header>
-                    <h1>Feedback</h1>
-                    <Styles.CloseButton onClick={onClose}>
-                        <FontAwesomeIcon icon={faTimes} />
-                    </Styles.CloseButton>
-                </Styles.Header>
-                <Styles.Content>
-                    <Styles.Form src='https://forms.gle/NGaRNuJcAtZ59G346' />
-                </Styles.Content>
-            </Styles.Container>
-        </Modal>
-    )
-
+  return (
+    <Modal isOpen={isOpen} onRequestClose={onClose} style={customStyles}>
+      <Styles.Container>
+        <Styles.Header>
+          <h1>Feedback</h1>
+          <Styles.CloseButton onClick={onClose}>
+            <FontAwesomeIcon icon={faTimes} />
+          </Styles.CloseButton>
+        </Styles.Header>
+        <Styles.Content>
+          <Styles.Form src="https://forms.gle/NGaRNuJcAtZ59G346" />
+        </Styles.Content>
+      </Styles.Container>
+    </Modal>
+  );
 }
